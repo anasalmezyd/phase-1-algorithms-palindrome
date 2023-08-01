e78a16983011d9f3ea19f06bd8e2d6b2f5c6c390
anasalmezyd <Anas.almziad@gmail.com>
2023-08-01 11:40:40 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
+// Function to check if the given word is a palindrome
 function isPalindrome(word) {
-  // Write your algorithm here
+  let start = 0;
+  let end = word.length - 1;
+
+  // Compare characters from both ends until start pointer is less than end pointer
+  while (start < end) {
+    // If characters at start and end are not equal, the word is not a palindrome
+    if (word[start] !== word[end]) {
+      return false;
+    }
+
+    // Move the pointers towards the middle
+    start++;
+    end--;
+  }
+
+  // If the loop completes, the word is a palindrome
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
+// Test Cases
+console.log(isPalindrome("racecar")); // Output: true
+console.log(isPalindrome("robot")); // Output: false
 
-/*
-  Add written explanation of your solution here
+/* 
+  Pseudocode:
+
+  isPalindrome(word):
+    start = 0
+    end = length of word - 1
+
+    while start < end:
+      if word[start] is not equal to word[end]:
+        return false
+
+      increment start
+      decrement end
+
+    return true
+
+  Explanation:
+
+  1. The function starts with two pointers, start and end, initialized to the beginning and end of the string word, respectively.
+  2. It enters a while loop that continues as long as the start pointer is less than the end pointer.
+  3. In each iteration, the function compares the character at the start pointer with the character at the end pointer. If they are not equal, it means the string is not a palindrome, and the function immediately returns false.
+  4. If the characters are equal, it increments the start pointer and decrements the end pointer to move towards the middle of the string for the next comparison.
+  5. The loop continues until the start and end pointers meet in the middle. If the loop completes without finding different characters, the string is a palindrome, and the function returns true.
+
 */
 
 // You can run `node index.js` to view these console logs
